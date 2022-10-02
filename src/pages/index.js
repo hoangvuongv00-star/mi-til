9d4e7735676e296f24c1a5e714f48b26659fc1db
hoangvuongv00-star <71877701+hoangvuongv00-star@users.noreply.github.com>
2022-10-02 20:41:32 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,16 +25,16 @@ function IndexPage({ location }) {
         </span>
       </h1>
       <p>Nguyen Le Anh Thu</p>
-      <p>Fish can swim, i as a human can not<p>
- <p>Fish can climb trees, i as a human can<p>
+      <p>Fish can swim, i as a human can not</p>
+ <p>Fish can climb trees, i as a human can</p>
 
- <p>Fish can fly, i as a human can but with support<p>
+ <p>Fish can fly, i as a human can but with support</p>
 
- <p>So cat is the mighty, but fish!<p>
+ <p>So cat is the mighty, but fish!</p>
 
- <p>Even though they love water, but its the water itself that creates the illusion of fish can not do anything but swim. That’s not true and i am here to prove it<p>
+ <p>Even though they love water, but its the water itself that creates the illusion of fish can not do anything but swim. That’s not true and i am here to prove it</>
 
- <p>I am a fish that can climb trees & hope to fly by itself one day!<p>
+ <p>I am a fish that can climb trees & hope to fly by itself one day!</p>
       </p>
       <Link to="/post/">
         <Button marginTop="35px">See all Posts</Button>
